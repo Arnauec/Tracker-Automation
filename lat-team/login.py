3b03274855abbdd089b630c4df0533556aaf402a
Arnauec <arnau@estebanell.com>
2024-05-13 20:02:51 +0200

    Run chrome with headless version

--- lat-team/login.py
+++ lat-team/login.py
@@ -20,6 +20,7 @@ password = os.getenv('LAT_PW')
 
 # Set up Chrome options
 options = Options()
+options.add_argument('--headless')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
 # Set up WebDriver
