8dd0b5f92fd402beb011d9a9aaf8acd087afaa9c
Arnauec <arnau@estebanell.com>
2024-05-13 19:13:10 +0200

    Adding explicit python3 rather than python on its own

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "$(date) - Script started" >> /app/app.log
-/usr/local/bin/python /app/hdolimpo/login.py
-/usr/local/bin/python /app/hdolimpo/check_ratio.py
-/usr/local/bin/python /app/hdolimpo/total_downloads.py
-/usr/local/bin/python /app/myanonamouse/login.py
-/usr/local/bin/python /app/myanonamouse/check_ratio.py
-/usr/local/bin/python /app/div-team/login.py
-/usr/local/bin/python /app/div-team/check_ratio.py
-/usr/local/bin/python /app/lat-team/login.py
-/usr/local/bin/python /app/lat-team/check_ratio.py
-/usr/local/bin/python /app/bye.py
+/usr/bin/python3 /app/hdolimpo/login.py
+/usr/bin/python3 /app/hdolimpo/check_ratio.py
+/usr/bin/python3 /app/hdolimpo/total_downloads.py
+/usr/bin/python3 /app/myanonamouse/login.py
+/usr/bin/python3 /app/myanonamouse/check_ratio.py
+/usr/bin/python3 /app/div-team/login.py
+/usr/bin/python3 /app/div-team/check_ratio.py
+/usr/bin/python3 /app/lat-team/login.py
+/usr/bin/python3 /app/lat-team/check_ratio.py
+/usr/bin/python3 /app/bye.py
 echo "$(date) - Script ended" >> /app/app.log
